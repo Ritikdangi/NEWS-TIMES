33e507711c72cb678cc71fd44ac2080b556f1b84
Ritik Dangi <ritikdangi445566@gmail.com>
2024-06-01 15:20:53 +0530

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      external: ['virtual:i18next-loader'],
+    },
+  }  
 });
